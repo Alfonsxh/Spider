392fc80d678c4c5a09ce076b1dbe50e49fb81195
Alfons <xiaohuihui100@gmail.com>
2018-12-24 23:58:58 +0800

    1 V0.1: add more movie links

--- DoubanMovieSpider/DoubanMovieSpider/spiders/DoubanMovieLinksSpider.py
+++ DoubanMovieSpider/DoubanMovieSpider/spiders/DoubanMovieLinksSpider.py
@@ -8,6 +8,7 @@
 import json
 import scrapy
 import traceback
+from urllib.parse import quote
 from items import DoubanMovieLinksSpiderItem
 
 headers = {
@@ -19,10 +20,16 @@ class DoubanMovieLinksSpider(scrapy.Spider):
     name = "DoubanMovieLinks"
 
     def start_requests(self):
-        movies_link_urls = "https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1&start={page}"
-        for page in range(10000):
-            # yield scrapy.Request(movies_link_urls.format(page=page * 20), headers=headers, callback=self.parse)
-            yield scrapy.Request(movies_link_urls.format(page=page * 20), callback=self.parse)
+        base_url = "https://movie.douban.com/j/new_search_subjects?sort=U&range=0,10&tags=%E7%94%B5%E5%BD%B1&start={page}&genres={type}"
+        type_list = ["剧情", "喜剧", "动作", "爱情", "科幻", "动画", "悬疑", "惊悚", "恐怖", "犯罪", "同性", "音乐", "歌舞", "传记", "历史", "战争", "西部", "奇幻", "冒险", "灾难", "武侠", "情色"]
+
+        movies_link_urls_list = [base_url.format(page="{page}", type=quote(t)) for t in type_list]
+        movies_link_urls_list.append("https://movie.douban.com/j/new_search_subjects?sort=S&range=0,10&tags=%E7%94%B5%E5%BD%B1&start={page}")
+
+        for movies_link_urls in movies_link_urls_list:
+            for page in range(1000):
+                # yield scrapy.Request(movies_link_urls.format(page=page * 20), headers=headers, callback=self.parse)
+                yield scrapy.Request(movies_link_urls.format(page=page * 20), callback=self.parse)
 
     def parse(self, response):
         try:
--- DoubanMovieSpider/run.py
+++ DoubanMovieSpider/run.py
@@ -5,10 +5,13 @@
 @time: 18-12-23 下午4:06
 @version: v1.0 
 """
+import time
 from scrapy.cmdline import execute
 
 if __name__ == '__main__':
     spider_app = 'DoubanMovieLinks'
     cmd = 'scrapy crawl {0}'.format(spider_app)
 
+    start_time = time.time()
     execute(cmd.split())
+    print("[*] use time: {}'s.".format(time.time() - start_time))
